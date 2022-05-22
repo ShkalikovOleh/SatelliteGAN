1c05d60ae3c1320f85f5bffc5654437832ed508f
Oleh Shkalikov <Shkalikov.Oleh@gmail.com>
2022-05-22 20:05:34 +0200

    Fix path joining and add NaN filtering for dataset

--- datasets/crops.py
+++ datasets/crops.py
@@ -1,3 +1,4 @@
+import os
 from glob import glob
 
 import numpy as np
@@ -10,10 +11,16 @@ from torchvision.transforms import ToTensor, Normalize, Compose
 
 class CropsDataset(Dataset):
 
-    def __init__(self, root_dir):
+    def __init__(self, root_dir, exclude_names=None):
         super().__init__()
 
-        self.files = glob(f'{root_dir}/*.tif')
+        self.files = glob(os.path.join(root_dir, '*.tif'))
+
+        if exclude_names is not None:
+            for name in exclude_names:
+                path = os.path.join(root_dir, name)
+                self.files.remove(path)
+
         self.norm = Compose([
             ToTensor(),
             Normalize((0.5, 0.5, 0.5, 0.5),
@@ -36,4 +43,4 @@ class CropsDataset(Dataset):
             tiff.GetRasterBand(5).ReadAsArray()).to(torch.long)
         mask = F.one_hot(classes)
 
-        return image, mask
+        return image, mask, self.files[idx]
--- models/discriminator.py
+++ models/discriminator.py
@@ -1,7 +1,4 @@
-from turtle import forward
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class PatchGAN(nn.Module):
